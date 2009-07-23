6991a880a130c5f98ee100e1e019a7e47d566996
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-07-22 21:39:14 -0600

    basic el file

---
+++ tony.el
@@ -0,0 +1,6 @@
+;; Colors
+(color-theme-zenburn)
+
+;; Cucumber
+(autoload 'feature-mode "feature-mode" "Mode for editing cucumber files" t)
+(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))

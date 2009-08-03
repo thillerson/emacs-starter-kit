66cc7086a51caec1d197ee7681c89b663f986937
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-08-03 17:22:24 -0600

    added some keybindings and yasnippets

--- tony.el
+++ tony.el
@@ -1,6 +1,15 @@
 ;; Colors
 (color-theme-zenburn)
 
+;; Snippets
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/tony/snippets")
+
 ;; Cucumber
 (autoload 'feature-mode "feature-mode" "Mode for editing cucumber files" t)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; Keybindings
+(global-set-key (kbd "M-s") 'save-buffer)
+(global-set-key (kbd "M-z") 'undo)

7cc267599b688bd3d1f162e613c23c1c4bd6cc20
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-09-16 14:57:06 -0600

    adding some color themes

--- tony.el
+++ tony.el
@@ -1,29 +1,30 @@
-;; Paths
+;; == Paths ==
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
-;; Sizing
+;; == Sizing ==
 (add-to-list 'default-frame-alist '(left . 0))
 (add-to-list 'default-frame-alist '(top . 0))
 (add-to-list 'default-frame-alist '(height . 55))
 (add-to-list 'default-frame-alist '(width . 200))
 
-;; Alpha
+;; == Alpha ==
 (add-to-list 'default-frame-alist '(alpha 93 75))
 
-;; Colors
+;; == Colors ==  
 (add-to-list 'load-path "~/.emacs.d/tony/color-theme")
 (require 'color-theme)
+
+;; A few more color themes
+(add-to-list 'load-path "~/.emacs.d/tony/color-themes")
+
+;; Set the color theme
 (eval-after-load "color-theme"
   '(progn
      (color-theme-initialize)
-     (color-theme-charcoal-black)))
-(setq my-color-themes (list 'color-theme-charcoal-black 'color-theme-billw
-                            `color-theme-blue-mood `color-theme-calm-forest
-                            `color-theme-dark-blue `color-theme-dark-blue2
-                            `color-theme-deep-blue `color-theme-gray30))
+     (color-theme-zenburn)))
 
-;; Snippets
+;; == Snippets ==
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/tony/snippets")
@@ -32,12 +33,12 @@
 (autoload 'feature-mode "feature-mode" "Mode for editing cucumber files" t)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
 
-;; Haskell
+;; == Haskell ==
 (load "~/.emacs.d/elpa-to-submit/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-;; Scala
+;; == Scala ==
 (add-to-list 'load-path "~/.emacs.d/tony/scala-mode")
 (require 'scala-mode-auto)
 
@@ -49,25 +50,25 @@
                (yas/minor-mode-on)
                ))
 
-;; Keybindings
+;; == Keybindings == 
 (global-set-key (kbd "M-s") 'save-buffer)
 (global-set-key (kbd "M-z") 'undo)
 (global-set-key (kbd "M-l") 'goto-line)
 
-;; Hooks
+;; ==  Hooks == 
 ;;(add-hook 'ruby-mode-hook 'whitespace-mode)
 (add-hook 'nxml-mode-hook (lambda ()
                             (flyspell-mode -1)))
 (add-hook 'nxhtml-mode-hook (lambda ()
                               (flyspell-mode -1)))
 
-;; Viper (Viper is a package for emacs rebels)
+;; ==  Viper (Viper is a package for emacs rebels) ==
 ;;(setq viper-mode t)
 ;;(require 'viper)
 ;; Viper mode will be enabled by Vimpulse:
 (require 'vimpulse)
 
-;;; rhtml-mode for rinari
+;; == rhtml-mode for rinari == 
 (add-to-list 'load-path "~/.emacs.d/tony/rhtml")
 (require 'rhtml-mode)
 (add-hook 'rhtml-mode-hook
@@ -75,11 +76,12 @@
 (add-hook 'rhtml-mode-hook
           (lambda () (flyspell-mode -1)))
 
-;; Textmate.el
+;; == Textmate.el ==
 (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
 (require 'textmate)
 (textmate-mode)
-;; PeepOpen.el
+
+;; == PeepOpen.el == 
 (add-to-list 'load-path "~/.emacs.d/vendor/")
 (require 'peepopen)
 (global-set-key (kbd "C-x C-o") 'peepopen-goto-file-gui)
@@ -87,5 +89,5 @@
 ;; Opens files in the existing frame instead of making new ones.
 (setq ns-pop-up-frames nil)
 
-;; Start the server
+;; == Start the server == 
 (server-start)
